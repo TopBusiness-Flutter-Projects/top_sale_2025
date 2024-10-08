9f23c90f4d56037e062a856e5e29bc86319a468e
TopBusiness <semicolonsales@gmail.com>
2024-10-08 12:32:20 -0700

    Thana Adel

---
+++ lib/features/delevery_order/screens/delevery_order_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/utils/app_colors.dart';
+class DeleveryOrderScreen extends StatelessWidget {
+  const DeleveryOrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: const Column(children: [
+
+      ],),
+    );
+  }
+}
