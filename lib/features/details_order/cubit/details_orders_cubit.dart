0a5ab90c259c3101e1933f13515ba1dc34980713
ahmedelsapagh <semicolonsales@gmmail.com>
2024-10-15 16:57:04 +0300

    aal doen

--- lib/features/details_order/cubit/details_orders_cubit.dart
+++ lib/features/details_order/cubit/details_orders_cubit.dart
@@ -13,14 +13,15 @@ class DetailsOrdersCubit extends Cubit<DetailsOrdersState> {
   DetailsOrdersCubit(this.api) : super(DetailsOrdersInitial());
   ServiceApi api;
   OrderDetailsModel? getDetailsOrdersModel;
-  List list = [0, 1, 2,3]; // 0 عرض سعر
-   // جديدة
+  List list = [0, 1, 2, 3]; // 0 عرض سعر
+  // جديدة
 
-     int page = 1;
-     void changePage(int index) {
-       page = index;
-       emit(ChangePageState());
-     }
+  int page = 1;
+  void changePage(int index) {
+    page = index;
+    emit(ChangePageState());
+  }
+
   TextEditingController moneyController = TextEditingController();
   void getDetailsOrders({required int orderId}) async {
     emit(GetDetailsOrdersLoadingState());
