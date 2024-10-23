0e5a86f8fe14667f4c3e7ff23bc70093e66cdcd1
TopBusiness <semicolonsales@gmail.com>
2024-10-23 11:41:52 -0700

    cubit handle

--- lib/features/attendance_and_departure/cubit/attendance_and_departure_cubit.dart
+++ lib/features/attendance_and_departure/cubit/attendance_and_departure_cubit.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:top_sale/core/remote/service.dart';
 import '../../../core/models/get_contract_model.dart';
@@ -12,6 +13,10 @@ class AttendanceAndDepartureCubit extends Cubit<AttendanceAndDepartureState> {
   TextEditingController reasonController = TextEditingController();
   GlobalKey<FormState> formKey  = GlobalKey<FormState>();
   GetContractModel ? contractDetails;
+  DateTime? selectedStartDate;
+  DateTime? selectedEndDate;
+  String? fromDate;
+  String? toDate;
   getContract() async {
     emit(GetContractLoading());
     final result = await api.getContract();
@@ -22,5 +27,44 @@ class AttendanceAndDepartureCubit extends Cubit<AttendanceAndDepartureState> {
         emit(GetContractLoaded());
       },
     );
+  }
+  void onSelectedDate(bool isStartDate, BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: isStartDate
+          ? (selectedStartDate ?? DateTime.now())
+          : (selectedEndDate ?? DateTime.now()),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(9999),
+    );
+
+    if (picked != null) {
+      if (isStartDate) {
+        selectedStartDate = picked;
+      } else {
+        selectedEndDate = picked;
+      }
+      updateDateStrings();
+      emit(DateChangedState());
+
+      // // Call this to fetch the updated data
+      // getHrDataHome();
+      // context.read<AdvancesAndDiscountsCubit>().getAttendence(
+      //     userId: context.read<AdvancesAndDiscountsCubit>().userIdd ?? "1",
+      //     context: context);
+    }
+  }
+  void updateDateStrings() {
+    fromDate = selectedStartDate != null
+        ? DateFormat('yyyy-MM-dd', 'en').format(selectedStartDate!)
+        : DateFormat('yyyy-MM-dd', 'en')
+        .format(DateTime.now()); // تاريخ اليوم كقيمة افتراضية
+
+    toDate = selectedEndDate != null
+        ? DateFormat('yyyy-MM-dd', 'en').format(selectedEndDate!)
+        : DateFormat('yyyy-MM-dd', 'en')
+        .format(DateTime.now()); // تاريخ اليوم كقيمة افتراضية
+
+    print('From date: $fromDate, To date: $toDate');
   }
  }
--- lib/features/attendance_and_departure/cubit/attendance_and_departure_state.dart
+++ lib/features/attendance_and_departure/cubit/attendance_and_departure_state.dart
@@ -3,6 +3,5 @@
  class AttendanceAndDepartureInitial extends AttendanceAndDepartureState {}
 class GetContractLoading extends AttendanceAndDepartureState {}
  class GetContractError extends AttendanceAndDepartureState {}
- class GetContractLoaded extends AttendanceAndDepartureState {
-
- }
+ class GetContractLoaded extends AttendanceAndDepartureState {}
+ class DateChangedState extends AttendanceAndDepartureState {}
